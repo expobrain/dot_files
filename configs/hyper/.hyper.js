a3108c9f09785429f8dc52cb4ba2dc5fa97f4856
Daniele Esposti <daniele.esposti@gmail.com>
2020-12-18 22:48:07 +0000

    Use stable Hyper.js

--- configs/hyper/.hyper.js
+++ configs/hyper/.hyper.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'canary',
+    updateChannel: 'stable',
 
     // default font size in pixels for all tabs
     fontSize: 12,
@@ -156,6 +156,6 @@ module.exports = {
   keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
-    'editor:break': 'esc',
+    // 'editor:break': 'esc',
   },
 };
